a7b22cdf1a5de2bc880b117fd6cf6a28477becbf
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2025-10-30 22:26:15 +0300

    feat: solve task Delete digits

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../lib');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  const strN = n.toString();
+  let maxNumber = 0;
+
+  for (let i = 0; i < strN.length; i += 1) {
+    const newNumber = parseInt(strN.slice(0, i) + strN.slice(i + 1), 10);
+    maxNumber = Math.max(maxNumber, newNumber);
+  }
+
+  return maxNumber;
 }
 
 module.exports = {
